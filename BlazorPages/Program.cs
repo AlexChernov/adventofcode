13ade76b191c90582f179e466de77457b7d3286f
Alex Chernov <chernovalexeyolegovich@gmail.com>
2020-05-09 20:56:32 +0300

    Remove blazorstrap dependency

--- BlazorPages/Program.cs
+++ BlazorPages/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Threading.Tasks;
-using BlazorStrap;
 using Microsoft.AspNetCore.Blazor.Hosting;
 
 namespace AdventOfCode
@@ -9,7 +8,6 @@ namespace AdventOfCode
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.Services.AddBootstrapCss();
             builder.RootComponents.Add<App>("app");
 
             await builder.Build().RunAsync();
