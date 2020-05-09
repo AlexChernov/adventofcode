ab6fca170c9a615f5ef9dad1faa3a4f95613ac4e
Alex Chernov <chernovalexeyolegovich@gmail.com>
2020-05-09 17:19:43 +0300

    Implement Day 1 of event 2015

---
+++ Event2015/Day1/Day1.cs
@@ -0,0 +1,60 @@
+﻿namespace AdventOfCode.Solutions.Event2015.Day1
+{
+    using System;
+    using System.Collections.Generic;
+    using AdventOfCode.Solutions.Common;
+
+    /// <summary>
+    /// Incapsulates logic of the Day 1.
+    /// </summary>
+    public class Day1 : IAdventOfCodeDayRunner
+    {
+        /// <inheritdoc/>
+        public bool HaveVisualization() => false;
+
+        /// <inheritdoc/>
+        public IEnumerable<string> RunTask1(string input, bool shouldVisualise)
+        {
+            var floor = 0;
+            foreach (var instruction in input)
+            {
+                if (instruction == '(')
+                {
+                    ++floor;
+                }
+                else
+                {
+                    --floor;
+                }
+            }
+
+            yield return floor.ToString();
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<string> RunTask2(string input, bool shouldVisualise)
+        {
+            var floor = 0;
+            var index = 0;
+            for (index = 0; index < input.Length; ++index)
+            {
+                var instruction = input[index];
+                if (instruction == '(')
+                {
+                    ++floor;
+                }
+                else
+                {
+                    --floor;
+                }
+
+                if (floor == -1)
+                {
+                    break;
+                }
+            }
+
+            yield return (index + 1).ToString();
+        }
+    }
+}
