e79b8dcbd4be2d2ba71fabde64486bb0235ae48c
Alex Chernov <chernovalexeyolegovich@gmail.com>
2020-05-09 17:19:52 +0300

    Implement Day 2 if event 2015

---
+++ Event2015/Day2/Day2.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode.Solutions.Event2015.Day2
+{
+    using System;
+    using System.Collections.Generic;
+    using AdventOfCode.Solutions.Common;
+
+    /// <summary>
+    /// Incapsulates logic of the Day 1.
+    /// </summary>
+    public class Day2 : IAdventOfCodeDayRunner
+    {
+        /// <inheritdoc/>
+        public bool HaveVisualization() => false;
+
+        /// <inheritdoc/>
+        public IEnumerable<string> RunTask1(string input, bool shouldVisualise)
+        {
+            var lines = input.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            yield return 0.ToString();
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<string> RunTask2(string input, bool shouldVisualise)
+        {
+            var lines = input.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            yield return 0.ToString();
+        }
+    }
+}
